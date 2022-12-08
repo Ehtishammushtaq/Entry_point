3d7f0c56f936f62d08cc7f5f603567bb1aaf07be
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-12-08 04:03:02 -0600

    links

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Resumeupdated from './components/files/Resumeupdated.pdf'
 import Box from './components/box';
 import './App.css';
 import Profile from './components/profile';
+import Weather from './components/weather';
+
 function App() {
   return (
     
    <div>
      <Profile/>
+      <Weather/>
      <Box text='LINKDIN' href='https://www.linkedin.com/in/ehtishammushtaq/'/>
      <Box text='PROJECTS' href='https://my-projects-eta.vercel.app/'/>
      <Box text='RESUME' href={Resumeupdated}/>
---
+++ src/components/weather.js
@@ -0,0 +1,1 @@
+//this is an app component that will display the a small black box with dummy text in it
