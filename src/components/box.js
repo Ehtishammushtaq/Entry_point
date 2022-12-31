fb1c741106877276d781cd4c8f7d4c12c74927fd
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-12-31 03:01:47 -0600

    small stylystic changes

--- src/components/box.js
+++ src/components/box.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from '../components/box.module.css';
+
 function Box(props) {
    
-   
+      
   return(
     
-      <a className={styles.box} href={props.href}  target="_blank" rel="noopener noreferrer" > 
+      <a className={styles.box} href={props.href} rel="noopener noreferrer" > 
        <span>
         {props.text}
        </span>
-
-
-
       </a>
     
    
